681ad5126fc54276637d5f51c5b432bee7cc8ecf
HiLucred <lucred@protonmail.com>
2024-10-01 21:53:40 -0300

    fix: Edit order recipient use case with admin role

--- src/domain/application/use-cases/edit-order-recipient.spec.ts
+++ src/domain/application/use-cases/edit-order-recipient.spec.ts
@@ -2,8 +2,6 @@ import { InMemoryOrdersRepository } from 'test/repositories/in-memory-orders-rep
 import { EditOrderUseCase } from './edit-order-recipient'
 import { InMemoryRecipientsRepository } from 'test/repositories/in-memory-recipients-repository'
 import { makeOrder } from 'test/factories/make-order'
-import { Recipient } from '@/domain/enterprise/entitys/recipient'
-import { Address } from '@/domain/enterprise/value-objects/address'
 
 let inMemoryRecipientsRepository: InMemoryRecipientsRepository
 let inMemoryOrdersRepository: InMemoryOrdersRepository
@@ -19,41 +17,34 @@ describe('Edit Order Recipient Use Case', () => {
   })
 
   it('should be able to edit a order', async () => {
-    const order = makeOrder({
-      recipient: Recipient.create({
-        name: 'Gabriel',
-        address: new Address({
-          zipCode: '808080',
-          street: 'R. Any Street',
-          city: 'Any City',
-          neighborhood: 'Any Neighborhood',
-          number: 'Any number',
-          state: 'Any state',
-        }),
-      }),
-    })
+    const order = makeOrder()
     inMemoryOrdersRepository.create(order)
 
     const result = await sut.execute({
+      userRole: 'admin',
       orderId: order.id.toString,
       recipient: { name: 'John Doe', city: 'Rio Branco' },
     })
 
     expect(result.isRight()).toBeTruthy()
-    expect(inMemoryOrdersRepository.orders[0].recipient.name).toEqual(
-      'John Doe',
-    )
-    expect(inMemoryOrdersRepository.orders[0].recipient.address.city).toEqual(
-      'Rio Branco',
-    )
-    expect(
-      inMemoryOrdersRepository.orders[0].recipient.address.neighborhood,
-    ).toEqual('Any Neighborhood')
-
+    expect(inMemoryRecipientsRepository.recipients[0].name).toEqual('John Doe')
     if (result.isRight()) {
       expect(inMemoryRecipientsRepository.recipients[0]).toEqual(
         result.value.order.recipient,
       )
     }
+  })
+
+  it('should not be able to edit a order without admin role', async () => {
+    const order = makeOrder()
+    inMemoryOrdersRepository.create(order)
+
+    const result = await sut.execute({
+      userRole: 'courier',
+      orderId: order.id.toString,
+      recipient: { name: 'John Doe', city: 'Rio Branco' },
+    })
+
+    expect(result.isLeft()).toBeTruthy()
   })
 })
--- src/domain/application/use-cases/edit-order-recipient.ts
+++ src/domain/application/use-cases/edit-order-recipient.ts
@@ -3,11 +3,14 @@ import { OrdersRepository } from '../repositories/orders-repository'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { Order } from '@/domain/enterprise/entitys/order'
 import { Address } from '@/domain/enterprise/value-objects/address'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
 
 interface EditOrderUseCaseRequest {
+  userRole: string
   orderId: string
   recipient: Partial<{
     name: string
+    phoneNumber: string
     zipCode: string
     street: string
     number: string
@@ -23,20 +26,21 @@ export class EditOrderUseCase {
   constructor(private readonly ordersRepository: OrdersRepository) {}
 
   async execute({
+    userRole,
     orderId,
     recipient,
   }: EditOrderUseCaseRequest): Promise<EditOrderUseCaseResponse> {
+    if (userRole !== 'admin') {
+      return left(new NotAllowedError())
+    }
+
     const order = await this.ordersRepository.findById(orderId)
 
     if (!order) {
       return left(new ResourceNotFoundError())
     }
 
-    const updatedRecipientName = recipient.name ?? order.recipient.name
-
-    const {
-      recipient: { address: previousAddress },
-    } = order
+    const previousAddress = order.recipient.address
 
     const updatedRecipientAddress = new Address({
       zipCode: recipient.zipCode ?? previousAddress.zipCode,
@@ -47,7 +51,12 @@ export class EditOrderUseCase {
       state: recipient.state ?? previousAddress.state,
     })
 
+    const updatedRecipientName = recipient.name ?? order.recipient.name
+    const updatedrecipientPhoneNumber =
+      recipient.phoneNumber ?? order.recipient.phoneNumber
+
     order.recipient.name = updatedRecipientName
+    order.recipient.phoneNumber = updatedrecipientPhoneNumber
     order.recipient.address = updatedRecipientAddress
 
     await this.ordersRepository.save(order)

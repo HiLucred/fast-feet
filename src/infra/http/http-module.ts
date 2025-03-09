72a2f44ac9996197768821e255e12882cc32833b
Gabriel <lucred@protonmail.com>
2025-03-09 13:16:06 -0300

    fix: Vitest conft

--- src/infra/http/http-module.ts
+++ src/infra/http/http-module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AuthenticateAdminController } from './controllers/authenticate-admin.controller'
 import { AuthenticateAdminUseCase } from '@/domain/app/application/use-cases/authenticate-admin'
-import { DatabaseModule } from '../database/prisma/database.module'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { CreateCourierController } from './controllers/create-courier.controller'
 import { CreateCourierUseCase } from '@/domain/app/application/use-cases/create-courier'
 import { AuthenticateCourierController } from './controllers/authenticate-courier.controller'
 import { AuthenticateCourierUseCase } from '@/domain/app/application/use-cases/authenticate-courier'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
